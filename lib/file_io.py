800f49f4553fe323ce022dcfe3f397e9709437c6
Wambui <wambuikaranja78@gmail.com>
2025-08-14 19:45:29 +0300

    creates IO

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+   
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
+
+write_file("logfile", "Log 1: 5 bananas added\n")
+append_file("logfile", "Log 2: 3 bananas subtracted\n")
+
+print(read_file("logfile"))
+
